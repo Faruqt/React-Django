cede0845a2abace3b200f1e10ba7d9725458eeab
Faruq <faruqabdulsalam@yahoo.com>
2021-11-13 01:05:58 +0100

    Create App.jsx component

---
+++ project1/frontend/src/components/App.jsx
@@ -0,0 +1,18 @@
+import Note from "./Notes"
+import Header from "./Header"
+import Footer from "./Footer"
+
+function App() {
+    
+  return (
+    <div className='App'>
+
+      <Header />
+      <Note />
+      <Footer />
+
+    </div>
+  );
+}
+
+export default App;
